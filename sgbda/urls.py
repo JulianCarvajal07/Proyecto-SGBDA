33ab017fb5a22319a31b63b702dbbab73d5af657
Julian Andres Carvajal Tabares <juliancarvajaltabares@gmail.com>
2026-05-13 16:13:57 -0500

    implementacion de boton para cerrar sesion

--- sgbda/urls.py
+++ sgbda/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login_usuario, name='login'),
     path('inicio/', views.bases_de_datos, name='bases_de_datos'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
