da1f011933f6d055c5437ba24ad2885f7ce8872b
Julian Andres Carvajal Tabares <juliancarvajaltabares@gmail.com>
2026-07-16 14:08:10 -0500

    reubicacion de archivo info_motores.html, se coloco dentro del directorio contenidos-DBA

--- sgbda/views/informacion.py
+++ sgbda/views/informacion.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 def informacion_motores(request):
 
-    return render(request, 'paginas/info_motores.html')
+    return render(request, 'paginas/contenidos-DBA/info_motores.html')
 
 def mantenimientos_SqlServer(request):
 
