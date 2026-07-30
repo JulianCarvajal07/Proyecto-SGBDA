b35822cfedf36fc07f2410769cbe2d05e08d142d
Julian Andres Carvajal Tabares <juliancarvajaltabares@gmail.com>
2026-07-30 16:17:29 -0500

    creacion de template para gestionar conexiones por SSH

--- sgbda/urls.py
+++ sgbda/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from .views.bienvenida import bienvenida, inicio
 from .views.usuarios import registro_usuarios, listar_usuarios, eliminar_usuario
 from .views.login import login_usuario
-from .views.conexiones import registro_conexion, listar_conexiones, eliminar_conexion
+
+from .views.conexion_bd import registro_conexion, listar_conexiones, eliminar_conexion
+from .views.conexion_ssh import listar_conexiones_ssh
+
 from .views.inventario import listar_inventario, detalles_instancia, eliminar_instancia, stream_actualizacion, exportar_excel
 from .views.builds import listar_builds, actualizar_builds
 from .views.clientes import listar_clientes, registro_cliente, modificar_cliente, eliminar_cliente
@@ -24,6 +27,8 @@ urlpatterns = [
     path('listar_conexiones/', listar_conexiones, name='listar_conexiones'),
     path('registro_conexion/', registro_conexion, name='registro_conexion'),
     path('eliminar_conexion/<int:id>/', eliminar_conexion, name='eliminar_conexion'),
+
+    path('listar_conexiones_ssh/', listar_conexiones_ssh, name='listar_conexiones_ssh'),
 
     path('listar_clientes/', listar_clientes, name='listar_clientes'),
     path('registro_cliente/', registro_cliente, name='registro_cliente'),
---
+++ sgbda/views/conexion_ssh.py
@@ -0,0 +1,141 @@
+import pyodbc
+
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from sgbda.models import conexion
+
+def listar_conexiones_ssh(request):
+    
+    # todas_conexiones = conexion.objects.all()
+    
+    return render(request, 'paginas/conexiones/conexion_ssh.html', {
+        #"conexiones": todas_conexiones
+    })
+
+"""
+def registro_conexion(request):
+
+    if request.method == 'POST':
+
+        motor = request.POST.get('motor').strip()
+        ip_servidor = request.POST.get('ip_servidor').strip()
+        puerto = request.POST.get('puerto').strip()
+        autenticacion = request.POST.get('autenticacion').strip()
+        usuario = request.POST.get('usuario', '').strip()
+        password = request.POST.get('contraseña', '').strip()
+
+        # ==========================================
+        # VALIDACIONES GENERALES
+        # ==========================================
+        if not all([
+            motor.strip(),
+            ip_servidor.strip(),
+            puerto.strip(),
+            autenticacion.strip()
+        ]):
+            messages.error(request,"IP, puerto y autenticación son obligatorios")
+            return redirect ('listar_conexiones')
+        
+        # ==========================================
+        # VALIDAR CREDENCIALES
+        # ==========================================
+        if autenticacion in ["Database Authentication"]:
+
+            if not all([usuario, password]):
+
+                messages.error(
+                    request,
+                    "Usuario y contraseña son obligatorios"
+                )
+
+                return redirect('listar_conexiones')
+            
+        if conexion.objects.filter(
+            ip_servidor=ip_servidor, 
+            puerto=puerto, 
+            motor=motor
+        ).exists():
+            messages.error(request, "Esta conexion ya existe en la base de datos")
+            return redirect ('listar_conexiones')
+        
+        try:
+
+            # =====================================================
+            # STRING DE CONEXION
+            # =====================================================
+            if motor == "SQL SERVER":
+
+                if autenticacion == "Database Authentication":
+
+                    conn_str = (
+                        "DRIVER={ODBC Driver 18 for SQL Server};"
+                        f"SERVER={ip_servidor},{puerto};"
+                        f"UID={usuario};"
+                        f"PWD={password};"
+                        "TrustServerCertificate=yes;"
+                    )
+            
+            if motor == "POSTGRESQL":
+
+                if autenticacion == "Database Authentication":
+
+                    conn_str = (
+                        "DRIVER={PostgreSQL Unicode};"
+                        f"SERVER={ip_servidor},{puerto};"
+                        f"UID={usuario};"
+                        f"PWD={password};"
+                        "TrustServerCertificate=yes;"
+                    )
+
+            # =====================================================
+            # TEST DE CONEXION
+            # =====================================================
+
+            with pyodbc.connect(conn_str):
+                pass
+
+            # =====================================================
+            # SI CONECTA -> GUARDAR CONFIGURACION
+            # =====================================================
+
+            conexion.objects.create(
+                motor = motor,
+                ip_servidor=ip_servidor,
+                puerto=puerto,
+                tipo_autenticacion=autenticacion,
+                usuario=usuario,
+                password_encriptado=password
+            )
+
+            # =====================================================
+            # CERRAR CONEXION
+            # =====================================================
+
+            messages.success(
+                request,
+                'La conexion fue existosa'
+            )
+
+        except Exception as e:
+
+            print({str(e)})
+
+            messages.error(
+                request,
+                f'Error de conexion: {str(e)}'
+            )
+
+        return redirect('listar_conexiones')
+
+    return render(request, 'paginas/conexiones/conexion_bd.html')
+
+def eliminar_conexion(request, id):
+
+    if request.method == "POST":
+        eliminar = get_object_or_404(conexion, id=id)
+        eliminar.delete()
+        messages.success(request, "conexion eliminada correctamente")
+
+    return redirect("listar_conexiones")
+
+"""
